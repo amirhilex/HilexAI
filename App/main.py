4885b18f70ead5f03c1a29f8643bd6852393abba
amirhilex <69090935+amirhilex@users.noreply.github.com>
2025-10-30 18:58:07 +0330

    Update main.py

--- App/main.py
+++ App/main.py
@@ -3,8 +3,11 @@ from App.adapters.twikit_scraper import TwikitScraper
 from App.adapters.csv_repository import CSVRepository
 
 async def main():
+    keyword = input("Enter search keyword: ").strip()
+    limit = int(input("Enter the maximum number of tweets to fetch (limit): ").strip())
+    count = int(input("Enter the number of tweets per request (count): ").strip())
     scraper = TwikitScraper()
-    tweets = await scraper.search_tweets("btc", limit=100)
+    tweets = await scraper.search_tweets(keyword, limit=limit, count=count)
     repo = CSVRepository("tweets.csv")
     repo.save_many(tweets)
 
