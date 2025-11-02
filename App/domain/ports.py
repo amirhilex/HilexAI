36d80fdeb72dead5416603b776367ab2b95c60c5
Amirhilex <69090935+amirhilex@users.noreply.github.com>
2025-11-02 10:56:04 +0330

    Delete App directory

--- App/domain/ports.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-from .entities import Tweet
-
-class ScraperPort(ABC):
-    @abstractmethod
-    async def search_tweets(self, keyword: str, limit: int) -> List[Tweet]:
-        pass
